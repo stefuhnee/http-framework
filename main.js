4420abd143bfbf876ec6e2e4ba3997df7b91f966
Stefanie Hansen <stefuhnee@gmail.com>
2016-05-20 09:02:54 -0700

    reorganized

---
+++ main.js
@@ -0,0 +1,5 @@
+'use strict';
+
+const server = require('./lib/server');
+const router = require('./lib/router');
+const handler = require('./lib/handlers');
--- server.js
+++
@@ -1,3 +0,0 @@
-'use strict';
-
-const http = require('http');
