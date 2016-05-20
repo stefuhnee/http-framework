6c6223b5447c4f2f017e4548dbdaf1f89ccf2531
Stefanie Hansen <stefuhnee@gmail.com>
2016-05-19 18:03:51 -0700

    basic file structure set up

---
+++ test/test.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const chai = require('chai');
+const expect = require('chai').expect;
+const chaiHTTP = require('chai-http');
+chai.use(chaiHTTP);
+const request = chai.request;
+const fs = require('fs');
+let oldFiles = [];
+let newFiles = [];
+require('../server.js');
